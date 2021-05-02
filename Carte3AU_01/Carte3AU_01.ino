c0239f8b671a680d6bd7fad850097560f9c6f158
Emile Costa <emile.costa@gmail.com>
2021-05-02 14:22:38 +0200

    Modify comments

--- Carte3AU_01/Carte3AU_01.ino
+++ Carte3AU_01/Carte3AU_01.ino
@@ -7,35 +7,40 @@
 #define LED_B 17
 
 // the setup function runs once when you press reset or power the board
-void setup() {
+void setup()
+{
   pinMode(LED_R, OUTPUT);
   pinMode(LED_G, OUTPUT);
   pinMode(LED_B, OUTPUT);
+  digitalWrite(LED_R, LOW); // turn the LED Red Off
+  digitalWrite(LED_G, LOW); // turn the LED Green Off
+  digitalWrite(LED_B, LOW); // turn the LED Blue Off
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_R, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                  // wait for a second
-  digitalWrite(LED_R, LOW);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_G, HIGH);    // turn the LED off by making the voltage LOW
-  delay(1000);                  // wait for a second
-  digitalWrite(LED_G, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(LED_B, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                 // wait for a second
-  digitalWrite(LED_B, LOW);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_R, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_G, HIGH);    // turn the LED off by making the voltage LOW
-  delay(1000);                  // wait for a second
-  digitalWrite(LED_R, LOW);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_B, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                  // wait for a second
-  digitalWrite(LED_G, LOW);
-  digitalWrite(LED_R, HIGH);    // turn the LED off by making the voltage LOW
-  delay(1000);                  // wait for a second
-  digitalWrite(LED_G, HIGH);
-  delay(2000);
-  digitalWrite(LED_R, LOW);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_G, LOW);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_B, LOW);    // turn the LED off by making the voltage LOW
+void loop()
+{
+  digitalWrite(LED_R, HIGH); // turn the LED Red On
+  delay(1000);               // wait for a second
+  digitalWrite(LED_R, LOW);  // turn the LED Red Off
+  digitalWrite(LED_G, HIGH); // turn the LED Green On
+  delay(1000);               // wait for a second
+  digitalWrite(LED_G, LOW);  // turn the LED Green Off
+  digitalWrite(LED_B, HIGH); // turn the LED Blue On
+  delay(1000);               // wait for a second
+  digitalWrite(LED_B, LOW);  // turn the LED Blue Off
+  digitalWrite(LED_R, HIGH); // turn the LED Red On
+  digitalWrite(LED_G, HIGH); // turn the LED Green On
+  delay(1000);               // wait for a second
+  digitalWrite(LED_R, LOW);  // turn the LED Red Off
+  digitalWrite(LED_B, HIGH); // turn the LED Blue On
+  delay(1000);               // wait for a second
+  digitalWrite(LED_G, LOW);  // turn the LED Green Off
+  digitalWrite(LED_R, HIGH); // turn the LED Red On
+  delay(1000);               // wait for a second
+  digitalWrite(LED_G, HIGH); // turn the LED Green On
+  delay(2000);               // wait for two seconds
+  digitalWrite(LED_R, LOW);  // turn the LED Red Off
+  digitalWrite(LED_G, LOW);  // turn the LED Green Off
+  digitalWrite(LED_B, LOW);  // turn the LED Blue Off
 }
